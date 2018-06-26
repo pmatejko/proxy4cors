6d4e31f184dbafd0b680e5ab3223ddb4c2d8f2a8
Paweł Matejko <matejko.pawel@gmail.com>
2018-06-26 14:54:53 +0200

    Initial commit

---
+++ src/main/java/io/github/pmatejko/proxy4cors/Proxy4CorsApplication.java
@@ -0,0 +1,12 @@
+package io.github.pmatejko.proxy4cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proxy4CorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proxy4CorsApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/pmatejko/proxy4cors/Proxy4CorsApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.pmatejko.proxy4cors;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Proxy4CorsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
